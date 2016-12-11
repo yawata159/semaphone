3d6fb43f7c2928c61f4ac58c9a5417513d2fa11f
yawata159 <seijiyawata@gmail.com>
2016-12-11 13:59:55 -0500

    Finish control.c

--- control.c
+++ control.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/sem.h>
-#include <string.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
 
 union semun {
     int val;
@@ -13,29 +18,63 @@ union semun {
 };
 
 int main(int argc, char *argv[]){
-    int semid;
+    if (argc != 2) 
+        return 0;
+    int semid, semc, shmid, shmc, fd;
     int key = ftok("makefile" , 22);
-    int sc;
-    if (strncmp(argv[1], "-c", strlen(argv[1])) == 0){
+    if (!strcmp(argv[1], "-c")){
+        // Create semaphore
         semid = semget(key, 1, IPC_CREAT | 0644);
-        printf("semaphore createdL %d\n", semid);
+        printf("semaphore created: %d\n", semid);
+
+        // Set up semaphore
         union semun su;
         su.val = 1;
+        semc = semctl(semid, 0, SETVAL, su);
+       
+        // Create shared memory
+        shmid = shmget(key, sizeof(int), IPC_CREAT | 0644);
+        printf("shared memory at: %d\n", shmid);
 
-        sc = semctl(semid, 0, SETVAL, su);
-        printf("value set: %d\n", su.val);
+        // Create file
+        fd = open("semaphone.txt", O_CREAT | O_TRUNC, 0644);
+        write(fd, 0, 0);
+        close(fd);
     }
-    else if (strncmp(argv[1], "-v", strlen(argv[1])) == 0){
+    else if (!strcmp(argv[1], "-v")){
+        // Display file
+        fd = open("semaphone.txt", O_RDONLY);
+        struct stat fileinfo;
+        stat("semaphone.txt", &fileinfo);
+        int size = fileinfo.st_size;
+        char content[size + 1];
+        content[size] = 0;
+        read(fd, content, size);
+        printf("%s", content);
+        close(fd);
+    }
+    else if(!strcmp(argv[1], "-r")){
+        // Remove semaphore
         semid = semget(key, 1, 0);
-        sc = semctl(semid, 0, GETVAL);
+        semc = semctl(semid, 0, IPC_RMID);
 
-        printf("semaphore value: %d\n",sc);
+        // Remove shared memory
+        shmid = shmget(key, sizeof(int), 0);
+        shmc = shmctl(shmid, IPC_RMID, 0);
+
+        // Display file
+        fd = open("semaphone.txt", O_RDONLY);
+        struct stat fileinfo;
+        stat("semaphone.txt", &fileinfo);
+        int size = fileinfo.st_size;
+        char content[size + 1];
+        content[size] = 0;
+        read(fd, content, size);
+        printf("%s", content);
+        close(fd);
     }
-    else if(strncmp(argv[1], "-r", strlen(argv[1])) == 0){
-        semid = semget(key, 1, 0);
-        sc = semctl(semid, 0, IPC_RMID);
-        printf("semaphore removed: %d\n", sc);
+    else {
+        printf("Not a valid argument\n");
     }
     return 0;
-
 }
